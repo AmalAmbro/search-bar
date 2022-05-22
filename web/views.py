ed3d4a2b6cf7b88118210b51e3ae93890c5b257b
Thomas Amal Ambrose <90627601+AmalAmbro@users.noreply.github.com>
2022-05-22 15:17:18 +0530

    Update views.py

--- web/views.py
+++ web/views.py
@@ -1,72 +1,57 @@
 from django.shortcuts import render
 
-from dictionary.models import WordMeaning, Words
-
 
 def index(request):
-    if request.method == 'POST':
-        search_word = request.POST.get('word').lower()
-        
-        word = Words.objects.filter(word = search_word)
+    
+    words = {
+        "abstract" : ["Theoretical","Notional","Intellectual"],
+        "competence" : ["Capability","Proficiency","Ability"],
+        "bystander " : ["Observer","Watcher","Onlooker"],
+        "dash" : ["Shatter","Destroy","Ruin"],
+        "stress" : ["Underscore","Accentuate","Point up"],
+        "perish" : ["Violent","Destroy","Decay"],
+        "definite" : ["Certain","Sure","Accurate"],
+        "describe" : ["Portray","Explain","Illustrate"],
+        "laugh" : ["Chuckle"],
+        "ideal" : ["Good","Model","Visionary"],
 
-        if word:
-            data = WordMeaning.objects.filter(word__in=word).order_by('priority')
-            
-            context = {
-                "title":"Search",
-                "datas":data,
-                "word" : search_word,
+    }
+
+    if request.method == 'POST':
+
+        word = request.POST.get('word')
+
+        if word :
+            word=word.lower()
+
+        synonyms = ""
+        if word in words.keys():
+            synonyms = words[word]
+            context =  {
+                "title":"Dictionary | Search",
+                "synonyms" : synonyms,
+                "synonym_length" : True,
+                "subheading":"Synonyms"
             }
                 
-            return render(request, 'index.html', context=context)
-        elif search_word == '':
-            data = "Thats an empty string please insert a proper word"
-            context = {
-                "title":"Search",
-                "missing":True,
-                "datas":data,
-            }
-                
-            return render(request, 'index.html', context=context)
         else:
-            data = f"'{search_word}' is not present in my dictionary, Please add it using the following link!!!"
-            context = {
-                "title":"Search",
-                "missing":True,
-                "datas":data,
+            synonyms = "No such word in my dictionary"
+            context =  {
+                "title":"Dictionary | Search",
+                "synonyms" : synonyms,
+                "synonym_length" : False,
+                "subheading":"Synonyms"
             }
-                
-            return render(request, 'index.html', context=context)
-    else:
-        context = {
-            "title":"Search",
-        }
-        
+        print("We got a request for the word : ", word)
+
+
         return render(request, 'index.html', context=context)
 
-def add(request):
-    if request.method == "POST":
-        word = request.POST.get('newWords').lower()
-        meaning = request.POST.get('addMeanings').lower()
-        priority = request.POST.get('priority')
-
-        new_word, created = Words.objects.get_or_create(
-            word = word
-        )
-        print(new_word, created)
-
-        WordMeaning.objects.create(
-            word = new_word,
-            meaning = meaning,
-            priority = priority
-        )
-        context = {
-            "title":"Add New"
-        }
-        return render(request, 'dict.html', context=context)
-
     else:
-        context = {
-            "title":"Add New"
-        }
-        return render(request, 'dict.html', context=context)
+        context =  {
+                "title":"Dictionary | Search",
+                "synonyms" : "",
+                "synonym_length" : False,
+                "subheading":""
+            }
+        return render(request, 'index.html', context=context)
